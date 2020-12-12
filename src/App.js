8cd8fc0a6f5c49bc3893a54fd0b370e69f8fd402
damon5378 <iosifov.konstantin@gmail.com>
2020-12-13 00:14:39 +0300

    AboutSection added styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyles from "./components/GlobalStyled";
 import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <AboutMe />
     </div>
   );
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,31 +1,65 @@
 import React from "react";
 import helen from "../img/helen.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
-            <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          <Hide>
             <h2>
-              your <span>dreams</span>
+              Самая <span>очаровательная</span>
             </h2>
-          </div>
-          <div className="hide">
-            <h2>come true.</h2>
-          </div>
+          </Hide>
+          <Hide>
+            <h2>девушка русскоязычного</h2>
+          </Hide>
+          <Hide>
+            <h2 className="additional_class">
+              АСМР на <span className="youtube">Youtube</span>
+            </h2>
+          </Hide>
         </div>
         <p>Contact me</p>
         <button>Contact me</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img style={{ borderRadius: 25 }} src={helen} alt="Cutie Helen" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-left: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  /* img {
+    width: 50%;
+    height: 50vh;
+    object-fit: cover;
+  } */
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
